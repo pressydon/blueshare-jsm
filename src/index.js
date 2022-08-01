bef66e18273f21007d9867ec1bebc82cef1813da
preciousCoder <67858458+pressydon@users.noreply.github.com>
2022-08-01 13:25:53 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
+
+
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+    <Router>
+
+        <App />
+
+    </Router>
+
+, document.getElementById('root'));
